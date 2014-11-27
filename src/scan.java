7fc58b1df6a573e904be314c428f013c0d24a800
Quentin Mallet <quentin.mallet@gmail.com>
2014-11-27 14:24:53 +0100

    learning the ins and outs for scanning and output to console

---
+++ src/scan.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class scan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir un mot :");
+		String str = sc.nextLine();
+		System.out.println("Vous avez saisi : " + str);
+
+	}
+
+}
