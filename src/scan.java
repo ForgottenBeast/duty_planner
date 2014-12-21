2dca85ca035231b552edc166c0912833ad7db3ea
Quentin Mallet <quentin.mallet@gmail.com>
2014-12-21 18:33:07 +0100

    added the base xls file for initialising the database and first steps were made to create a mem database

--- src/scan.java
+++ src/scan.java
@@ -1,7 +1,11 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.io.File; 
+import java.io.IOException;
+import java.util.Date; 
+import jxl.*; 
+import jxl.read.biff.BiffException;
 
 
 
@@ -15,6 +19,15 @@ public class scan {
 		 try {
 			Connection c = DriverManager.getConnection("jdbc:hsqldb:mem:gardedb", "SA", "");
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 try {
+			Workbook data = Workbook.getWorkbook(new File("data.xls"));
+		} catch (BiffException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
